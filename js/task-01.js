3a9d94f390af2e18b452451105620da6ff656001
Dpescar <128416673+Dpescar@users.noreply.github.com>
2023-09-11 19:22:46 +0300

    main

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,14 @@
-'use strict';
-const listCategories = document.querySelectorAll('#categories li.item');
+"use strict";
+const listCategories = document.querySelectorAll("#categories li.item");
 const amountCategories = listCategories.length;
 
 console.log(`Number of categories: ${amountCategories}`);
 
-listCategories.forEach(category => {
-  const headtitleCategory = category.querySelector('h2');
+listCategories.forEach((category) => {
+  const headtitleCategory = category.querySelector("h2");
   const nameCategory = headtitleCategory.textContent;
 
-  const listElements = category.querySelectorAll('ul li');
+  const listElements = category.querySelectorAll("ul li");
   const amountElements = listElements.length;
 
   console.log(`Category: ${nameCategory}`);
